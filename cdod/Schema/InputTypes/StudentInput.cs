3316a048cf38c01096ab199cc401a61ce5cedbeb
Keplar-01 <vania8022@gmail.com>
2022-07-06 22:10:59 +0300

    Added mutations (Parent, School, Student, User) Added InputTypes(Parent, School, Student, User)

---
+++ cdod/Schema/InputTypes/StudentInput.cs
@@ -0,0 +1,13 @@
+﻿namespace cdod.Schema.InputTypes
+{
+    public class StudentInput
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Patronymic { get; set; }
+        public string? Descriotion { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public int SchoolId { get; set; }
+        public int ParentId { get; set; }
+    }
+}
--- cdod/Schema/Mutations/MutationUser.cs
+++ cdod/Schema/Mutations/MutationUser.cs
@@ -1,42 +1,44 @@
 ﻿using cdod.Schema.InputTypes;
+using cdod.Schema.Mutations;
 using cdod.Services;
 using cdodDTOs.DTOs;
 
 namespace cdod.Schema
 {
+    [ExtendObjectType(typeof(Mutation))]
     public class MutationUser
     {
        [UseDbContext(typeof(CdodDbContext))]
-       public async Task<UserDTO> CreateUser(UserInput userForm, [Service] CdodDbContext dbContext)
+       public async Task<UserDTO> CreateUser(UserInput userForm, [ScopedService] CdodDbContext dbContext)
        {
-            UserDTO user = new UserDTO()
-            {
-                Firstname = userForm.Firstname,
-                Lastname = userForm.Lastname,
-                Patronymic = userForm.Patronymic,
-                Address = userForm.Address,
-                PhoneNumber = userForm.PhoneNumber,
-                Email = userForm.Email,
-                Password = userForm.Password,
-                Birthday = userForm.Birthday,
-                Education = userForm.Education,
-                Inn = userForm.Inn,
-                Snils = userForm.Snils,
-                passportCode = userForm.passportCode,
-                passportDate = userForm.passportDate,
-                passportNo = userForm.passportNo,
-                passportIssue = userForm.passportIssue,
-            };
-            dbContext.Users.Add(user);
-            await dbContext.SaveChangesAsync();
-            ParentDTO parent = new ParentDTO() { UserId = user.Id };
-            dbContext.Parents.Add(parent);
-            await dbContext.SaveChangesAsync();
-            return user;
+                    UserDTO user = new UserDTO()
+                    {
+                        Firstname = userForm.Firstname,
+                        Lastname = userForm.Lastname,
+                        Patronymic = userForm.Patronymic,
+                        Address = userForm.Address,
+                        PhoneNumber = userForm.PhoneNumber,
+                        Email = userForm.Email,
+                        Password = userForm.Password,
+                        Birthday = userForm.Birthday,
+                        Education = userForm.Education,
+                        Inn = userForm.Inn,
+                        Snils = userForm.Snils,
+                        passportCode = userForm.passportCode,
+                        passportDate = userForm.passportDate,
+                        passportNo = userForm.passportNo,
+                        passportIssue = userForm.passportIssue,
+                    };
+                    dbContext.Users.Add(user);
+                    await dbContext.SaveChangesAsync();
+                    ParentDTO parent = new ParentDTO() { UserId = user.Id };
+                    dbContext.Parents.Add(parent);
+                    await dbContext.SaveChangesAsync();
+                    return user;
         }
 
         [UseDbContext(typeof(CdodDbContext))]
-        public async Task<UserDTO> UpdateUser(int id, UserInput userForm, [Service] CdodDbContext dbContext)
+        public async Task<UserDTO> UpdateUser(int id, UserInput userForm, [ScopedService] CdodDbContext dbContext)
         {
             UserDTO user = new UserDTO()
             {
@@ -63,7 +65,7 @@ namespace cdod.Schema
         }
 
         [UseDbContext(typeof(CdodDbContext))]
-        public async Task<bool> DeleteUser(int userId, [Service] CdodDbContext dbContext)
+        public async Task<bool> DeleteUser(int userId, [ScopedService] CdodDbContext dbContext)
         {
             UserDTO user = new UserDTO() { Id = userId };
             dbContext.Users.Remove(user);
